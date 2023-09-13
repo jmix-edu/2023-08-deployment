71191ee900d7ad8ad5dcac46b260d67ffdeb34cb
Gleb Gorelov <g.gorelov@haulmont.com>
2023-09-13 10:53:16 +0400

    Basic completed

--- src/main/java/com/company/jmixpm/screen/task/TaskBrowse.java
+++ src/main/java/com/company/jmixpm/screen/task/TaskBrowse.java
@@ -1,17 +1,18 @@
 package com.company.jmixpm.screen.task;
 
 import com.company.jmixpm.app.TaskImportService;
+import com.company.jmixpm.entity.Task;
 import io.jmix.ui.Notifications;
 import io.jmix.ui.component.Button;
 import io.jmix.ui.model.CollectionLoader;
 import io.jmix.ui.screen.*;
-import com.company.jmixpm.entity.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @UiController("pm_Task.browse")
 @UiDescriptor("task-browse.xml")
 @LookupComponent("tasksTable")
 public class TaskBrowse extends StandardLookup<Task> {
+
     @Autowired
     private TaskImportService taskImportService;
 
